d2bb525d26d1695649ed9d50e2fb27f12aab7f19
ew11160 <103226332+ew11160@users.noreply.github.com>
2024-11-07 17:41:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let express = require("express");
+let app = new express();                                                                     
+app.set("view engine","ejs")
+
+// set up database connection
+const knex = require("knex")({
+ client: "mysql",
+ connection: {
+  host:"concert-db-ch8.cr2gs24umuaj.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "password123",
+  database:"paradise-concerts",
+  port: 3306,
+ },
+});
+
+app.get("/",(req,res) => {
+ knex
+ .select()
+ .from("venues")
+ .then((result) => {
+   res.render("index", {aConcerts: result});
+ }); 
+});
+app.listen(3002);
