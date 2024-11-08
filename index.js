fd8ffe5eba8f6768ed8a31ed4ced320332a23340
AsanteLA <asante.laryea.akrong@gmail.com>
2024-11-08 01:55:41 +0000

    Updated Index.js file

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-require('dotenv').config();
-let express = require("express");
+et express = require("express");
 let app = new express();
 app.set("view engine","ejs");
 
 // set up database connection
 const knex = require("knex")({
-  client: "mysql",
-  connection: {
-  host: process.env.DB_ENDPOINT,
-  user: "admin",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 3306,
- },
+client: "mysql",
+connection: {
+host:"terraform-20241108005603449600000006.cj44a88eg9cv.us-east-2.rds.amazonaws.com",
+user: "admin",
+password: "LetsDeploy01",
+database:"Donuts",
+port: 3306,
+},
 });
 
 app.get("/",(req,res) => {
-  knex
-  .select()
-  .from("Products")
-  .then((result) => {
-    console.log(result);
-    res.render("index",{aDonutList:result});
-  });
+knex
+.select()
+.from("Products")
+.then((result) => {
+console.log(result);
+res.render("index",{aDonutList:result});
 });
-
+});
 app.listen(3000);
-
-
-
-
-
-
-
-
-
 
