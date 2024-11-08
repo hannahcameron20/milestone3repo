fb6cff50e9c3fbe775ca765e5282dbd83bd3c714
AsanteLA <asante.laryea.akrong@gmail.com>
2024-11-08 04:00:52 +0000

    Added packages for express, ejs and knex

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ app.set("view engine","ejs");
 
 // set up database connection
 const knex = require("knex")({
-client: "mysql",
-connection: {
-host:"terraform-20241108005603449600000006.cj44a88eg9cv.us-east-2.rds.amazonaws.com",
-user: "admin",
-password: "LetsDeploy01",
-database:"Donuts",
-port: 3306,
-},
+ client: "mysql",
+ connection: {
+ host:"terraform-20241108005603449600000006.cj44a88eg9cv.us-east-2.rds.amazonaws.com",
+ user: "admin",
+ password: "LetsDeploy01",
+ database:"Donuts",
+ port: 3306,
+ },
 });
 
 app.get("/",(req,res) => {
-knex
-.select()
-.from("Products")
-.then((result) => {
-console.log(result);
-res.render("index",{aDonutList:result});
-});
+ knex
+ .select()
+ .from("Products")
+ .then((result) => {
+ console.log(result);
+  res.render("index",{aDonutList:result});
+ });
 });
 app.listen(3000);
 
